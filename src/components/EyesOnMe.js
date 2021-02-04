3c4a9239095f7e30ace5ac4361e35f635f37d3e3
noelanimanini <noelani26@crystals-MacBook-Pro.local>
2021-02-04 10:06:04 -0800

    react-ing to this lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'; 
+
+export default class EyesOnMe extends React.Component {
+    focus = () => {
+        console.log('Good!!!') 
+    }
+
+    blur = () => {
+        console.log('HEY! EYES ON MEEEE')
+    }
+
+    render () {
+        return (
+            <div>
+                <button onFocus={this.focus} onBlur={this.blur} >Button</button>
+            </div>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+// Code Keypad Component Her
+import React from 'react';
+
+export default class Keypad extends React.Component {
+    keyUp = () => {
+        console.log('Entering Password')
+    }
+    render () {
+        return (
+            <div>
+                <input onKeyUp={this.keyUp} type="password"/>
+            </div>
+        )
+    }
+}
